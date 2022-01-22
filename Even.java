38096bd46492d0b74a6ea54281c9b886d59214dc
prajakta249 <97919410+prajakta249@users.noreply.github.com>
2022-01-22 16:17:14 +0530

    Create Even.java  file

---
+++ Even.java
@@ -0,0 +1,12 @@
+public class Even
+{
+          public static void  main(String []args)
+           {
+                    int i = 4;
+                   
+                    if(i%2==0)
+                    {
+                             System.out.println("Number is Even");
+                    }
+           }
+}
